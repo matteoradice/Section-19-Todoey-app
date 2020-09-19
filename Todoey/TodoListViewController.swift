8e3954b3cc9d734ba6ddfe2bc4fed9f7df642dca
Matteo Radice <matteoradice@gmail.com>
2020-09-19 17:42:31 +0200

    Add tableView delegate method to select and checkmark the selected cell.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -27,7 +27,18 @@ extension TodoListViewController {
         cell.textLabel!.text = itemArray[indexPath.row]
         return cell
     }
+}
+
+// MARK: - TableView delegate methods
+
+extension TodoListViewController {
     
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark}
+    }
     
 }
