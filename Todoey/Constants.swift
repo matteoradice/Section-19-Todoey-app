183dfeabfa080a20be9ea8d806a2202c72c23f10
Matteo Radice <matteoradice@gmail.com>
2020-09-19 17:25:43 +0200

    Configure cells and tableview.

---
+++ Todoey/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Matteo Radice on 19/09/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    
+    struct cells {
+        static let itemCell = "ToDoItemCell"
+    }
+    
+}
