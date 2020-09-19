134a2cd34642262e73dacb6f75cc843bc0e31aaf
Matteo Radice <matteoradice@gmail.com>
2020-09-19 18:08:06 +0200

    Add navigation button and collect input from the user to refresh the table.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -5,10 +5,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new to Todoey item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            // what will happen once the user clicks the add item button on UIAlert
+            // Missing a code piece to prevent empty input in the textfield by the user
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
         
     }
     
@@ -21,7 +41,6 @@ extension TodoListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cells.itemCell, for: indexPath)
         cell.textLabel!.text = itemArray[indexPath.row]
@@ -37,8 +56,7 @@ extension TodoListViewController {
         // print(itemArray[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark}
     }
-    
 }
